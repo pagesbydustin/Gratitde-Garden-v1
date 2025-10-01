4ed50b4cbb1622abc5f0bc10d396fd8128453476
Dustin Parker <pagesbydustin@gmail.com>
2025-10-01 05:10:08 +0000

    make tem brighter on over

--- src/components/overview/WordCloud.tsx
+++ src/components/overview/WordCloud.tsx
@@ -46,7 +46,7 @@ export function WordCloud({ data }: WordCloudProps) {
         return (
           <span
             key={adjective}
-            className="transition-all duration-300 ease-in-out hover:scale-110 hover:opacity-75"
+            className="transition-all duration-300 ease-in-out hover:scale-110 opacity-75 hover:opacity-100"
             style={{
               fontSize: `${fontSize}rem`,
               fontWeight: fontWeight,
