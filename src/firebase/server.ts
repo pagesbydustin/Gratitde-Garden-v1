9ee481f95f83c70df7c1fb2deb25b8fea99fea90
Dustin Parker <pagesbydustin@gmail.com>
2025-12-04 13:39:25 +0000

    Try fixing this error: `Runtime Error: Error: Service account object mus

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -6,10 +6,10 @@ import { getFirestore, Firestore } from 'firebase-admin/firestore';
 
 const firebaseAdminConfig: AppOptions = {
     credential: cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
+        project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+        client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
         // Correctly format the private key by replacing escaped newlines.
-        privateKey: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+        private_key: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     })
 };
 
